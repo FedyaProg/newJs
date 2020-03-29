1bffa2aee27bb131d00d7df1520ec4e09f9765e3
Feyda <ed.prog.test@gmail.com>
2020-03-29 15:09:09 +0500

    First commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-alert('Fucking git');
+let money = '$';
+let income = 'Прибыль';
+let addExpenses = 'расходы';
+let deposit = 'deposits';
+let mission = 'планы';
+let period = 'период';
+
+
+alert('trying to study git by spending ' + money);
+
+console.log(income + ' через програмирования');
+
 
 
