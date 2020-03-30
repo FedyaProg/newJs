631e23d4dcd739a080d774d86872d31245a15e3e
Feyda <ed.prog.test@gmail.com>
2020-03-30 21:09:58 +0500

    solve  tasks in lesson02

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
-let money = '$';
-let income = 'Прибыль';
-let addExpenses = 'расходы';
-let deposit = 'deposits';
-let mission = 'планы';
-let period = 'период';
+let money = 10000;
+let income = 'IT Center';
+let addExpenses = 'car, wife, rent';
+let deposit = false;
+let mission = 5000000;
+let period = 12;
 
+let budgetDay;
 
-alert('trying to study git by spending ' + money);
+budgetDay = Math.floor(money/30);
 
-console.log(income + ' через програмирования');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев ' + 'Цель заработать ' + '$' + mission);
+
+console.log(addExpenses.split(', '));
+
+console.log(budgetDay);
+
 
 
 
