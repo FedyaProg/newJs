b902e563ac59b1ec10cf483e81b18365c84b2a1a
Feyda <ed.prog.test@gmail.com>
2020-03-31 01:55:27 +0500

    create new brench, solve complex tasks in lesson02

---
+++ js/complex.js
@@ -0,0 +1,13 @@
+
+let num = 266219;
+mult = ('' + num).split('');
+// console.log(mult);
+p = 1;
+
+for(var i = 0; i < mult.length; i++) {
+    p *= mult[i];
+}
+console.log(p);
+p = p*p*p;
+console.log(('' + p).substring(0, 2))
+
