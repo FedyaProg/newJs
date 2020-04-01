fc7bef444643db795455dd9c8a6f42158da54bb6
Feyda <ed.prog.test@gmail.com>
2020-04-01 16:32:45 +0500

    finished complex_lesson03, only have one problem

--- js/complex.js
+++ js/complex.js
@@ -1,13 +1,54 @@
+'use strict';
 
-let num = 266219;
-mult = ('' + num).split('');
-// console.log(mult);
-p = 1;
+// FROM LESSON03 =========================
 
-for(var i = 0; i < mult.length; i++) {
-    p *= mult[i];
-}
-console.log(p);
-p = p*p*p;
-console.log(('' + p).substring(0, 2));
+// let question = prompt('Выберите языка "Eng" или "Rus"?');
+// let weekRus = ['понедельник', 'вторник', 'среда', 'Четверг', 'Пятница', 'Суббота', 'воскресенье'];
+// let weekEng = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
+
+
+
+// if( question == 'Rus' || question == 'rus') {
+//     console.log(weekRus);
+// } else if( question == 'Eng' || question == 'eng') {
+//     console.log(weekEng);
+// } else {
+//     console.log('Что то пошло не так');
+// }
+
+// switch(question) {
+//     case 'rus':
+//     case 'Rus':
+//         console.log(weekRus);
+//         break;
+//     case 'eng':
+//     case 'Eng':
+//         console.log(weekEng);
+//         break;
+//     default:
+//         console.log('Что то пошло не так');
+// }
+
+
+// let namePerson = prompt('Напишите свое имя');
+
+// (namePerson == 'Artem' || namePerson == 'Артем') ? console.log('Director') : 
+//     (namePerson == 'Maxim' || namePerson == 'Максим') ? console.log('преподаватель') :
+//         console.log('студент');
+
+
+
+
+// FROM LESSON 2 =========================
+// let num = 266219;
+// mult = ('' + num).split('');
+// // console.log(mult);
+// p = 1;
+
+// for(var i = 0; i < mult.length; i++) {
+//     p *= mult[i];
+// }
+// console.log(p);
+// p = p*p*p;
+// console.log(('' + p).substring(0, 2));
