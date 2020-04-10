a235bc7b5c964f467335d3b7b543665db63a15b1
Feyda <ed.prog.test@gmail.com>
2020-04-10 20:03:56 +0500

    finished lesson10 and new git problems

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+
+const start = document.getElementById('start'),
+    btnPlusAddExpenses = document.getElementsByTagName('button')[0],
+    btnPlusExpenses = document.getElementsByTagName('button')[1],
+    checkboxDeposit = document.querySelector('#deposit-check'),
+    additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
+    resultValue = document.querySelectorAll('.newForValue'),
+    budgetMonthValue = document.querySelector('.budget_month-value'),
+    salaryAmount = document.querySelector('.salary-amount'),
+    incomeTitle = document.querySelector('.income-title'),
+    incomeAmount = document.querySelector('.income-amount'),
+    expensesTitle = document.querySelector('.expenses-items .expenses-title'),
+    expensesAmount = document.querySelector('.expenses-amount'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    targetAmount = document.querySelector('.target-amount'),
+    periodSelect = document.querySelector('.period-select');
+
+
+
+
+// const firstTag = document.querySelector('#id_salary_title');
+
+// console.log(firstTag.getAttribute('title'));
+// firstTag.setAttribute('style', 'color: blue')
+// firstTag.className = 'new_class_name'
+// firstTag.classList.add('salary_amount');
+// firstTag.classList.remove('title');
+// firstTag.classList.toggle('title'); // will change false to true or true to false
+// console.log(firstTag.classList.contains('title'));  // true or false 
+// firstTag.style.fontSize = '10px';
+// document.body.style.backgroundColor = '#454545'
+
+// const too = document.querySelector('.salary');
+// const comp = getComputedStyle(too);
+// console.log(comp.fontSize);
+// console.log(comp.color);
+
+
+// const border = document.querySelector('.data');
+// const exact = border.querySelectorAll('.expenses-title');
+
+// console.log(exact);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
